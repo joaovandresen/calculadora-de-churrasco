b4ab4589a030deaff2abb5a417ab9fd1414bb134
João Victor <joao.vandresen.costa@escola.pr.gov.br>
2025-09-30 13:32:04 +0000

    vaquinha em C finalizado

--- vaquinha.c
+++ vaquinha.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
+
+
 // Variável global
 float totalArrecadado = 0;
 
-//Procedimento: adiciona a contribuição de um participante
+// Procedimento: adiciona a contribuição de um participante
 void adicionaContribuicao(float valor){
-    totalArrecadado +=valor;
-    printf("Contribuição de R$ %.2f adicionada! Total: R$ %.2f");
+    totalArrecadado += valor;
+    printf("Contribuição de R$ %.2f adicionada! Total: R$ %.2f\n", valor, totalArrecadado);
 }
 
-// Função: calcula a quantidade de carne por pessoa(0.5kg por pessoa)
-
-float calaculaCarne(int qtdPessoas){
+// Função: calcula a quantidade de carne por pessoa (0.5kg por pessoa)
+float calculaCarne(int qtdPessoas){
     return qtdPessoas * 0.5;
 }
 
 
-// Procedimento pra mostrar o resmuo
+// Procedimento pra mostrar o resumo
 void mostraResumo(int qtdPessoas){
     float carne = calculaCarne(qtdPessoas);
     float media = totalArrecadado / qtdPessoas;
     printf("\n===== RESUMO CHURRASCO =====\n");
-    printf("Participantes: %d\n",qtdPessoas);
-    printf("Carne necessária: %2f KG\n", carne);
+    printf("Participantes: %d\n", qtdPessoas);
+    printf("Carne necessária: %.2f KG\n", carne);
     printf("Total arrecadado: R$ %.2f\n", totalArrecadado);
     printf("Média por pessoa: R$ %.2f\n", media);
-    printf("===============================");
+    printf("=============================\n");
 }
+
+
 
 int main(){
     int pessoas;
     float valor;
-
     printf("Digite o número de participantes: ");
-    scanf("%d,&pessoas");
+    scanf("%d", &pessoas);
     
-    for (int i = 1 ; i<= pessoas; i++){
-        printf("Digite a contribuição do participante %d: R$");
+    for (int i = 1; i <= pessoas; i++){
+        printf("Digite a contribuição do participante %d: R$ ", i);
         scanf("%f", &valor);
         adicionaContribuicao(valor);
     }
-
-
     mostraResumo(pessoas);
+    return 0;
 }
